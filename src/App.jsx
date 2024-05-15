26641e8b141ebe8d7961e64bcdc9e883c6c00475
ArsalanAman009 <118374146+ArsalanAman009@users.noreply.github.com>
2024-05-15 01:11:37 -0700

    add hashroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './myComponents/appBar/Navbar';
 import Home from './myComponents/appBar/Home';
 import IndoorProducts from './myComponents/appBar/IndoorProducts';
